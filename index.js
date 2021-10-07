18bf656f27317304f2c46c9fdae64da6cb5d2c98
kelvynking <kelvyn.m.king@gmail.com>
2021-10-07 15:08:44 +1100

    Create a function updateEmployeeWithKeyAndValue which takes in 3 arguments and returns a new object which does not mutate the original object.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your solution in this file!
+const employee = {
+    name : "bob",
+    streetAddress: "1 Bourke Street"
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    return {...obj, ...{[key]: value}};
+    //alternative using Object.assign()
+    // return Object.assign({}, obj, {[key]:value});
+}
